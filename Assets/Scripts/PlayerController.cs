72562f63d20228dfc3a25fc37f08b219a879c88d
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-08-07 17:50:15 +0530

    Almost finish level 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float TouchMovementSpeed = 600f;
-    public Vector3 touchJumpForce;
+    //public Vector3 touchJumpForce;
 
     public float keyboardMovementSpeed = 50000f;
-    public float keyboardJumpForce = 10000f;
+    //public float keyboardJumpForce = 10000f;
 
     protected Joystick joystick;
     protected Rigidbody rb;
@@ -35,42 +35,45 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         rb.AddForce(move * Time.deltaTime * keyboardMovementSpeed);
 
+
+        // JUMPING HAS BEEN DISABLED FOR EASIER DEVELOPMENT OF GAME
+
         // For jumping, first check if there is any touch input
-        if (isGrounded)
-        {
-            // Now check if player is touching the ground
-            if (Input.touchCount > 0)
-            {
-                // Now, check if the touch is in the right half of the screen
-                if (Input.GetTouch(Input.touchCount - 1).position.x > Screen.width / 2)
-                    // Add force to the player in the y direction
-                    rb.AddForce(touchJumpForce);
-            }
+        //if (isGrounded)
+        //{
+        //    // Now check if player is touching the ground
+        //    if (Input.touchCount > 0)
+        //    {
+        //        // Now, check if the touch is in the right half of the screen
+        //        if (Input.GetTouch(Input.touchCount - 1).position.x > Screen.width / 2)
+        //            // Add force to the player in the y direction
+        //            rb.AddForce(touchJumpForce);
+        //    }
 
-            // When space bar is pressed, jump
-            Vector3 jump = new Vector3(0, Input.GetAxis("Jump"), 0);
-            rb.AddForce(jump * Time.deltaTime * keyboardJumpForce);
+        //    // When space bar is pressed, jump
+        //    Vector3 jump = new Vector3(0, Input.GetAxis("Jump"), 0);
+        //    rb.AddForce(jump * Time.deltaTime * keyboardJumpForce);
 
-        }
+        //}
 
     }
 
     // To detect collisions for player jumping
-    void OnCollisionEnter(Collision collision)
-    {
-        // If the player just touched a surface, isGrounded = true
-        if (collision.gameObject.tag == "Surface")
-        {
-            isGrounded = true;
-        }
-    }
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    // If the player just touched a surface, isGrounded = true
+    //    if (collision.gameObject.tag == "Surface")
+    //    {
+    //        isGrounded = true;
+    //    }
+    //}
 
-    // If player just exited touching a surface, isGrounded = false
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "Surface")
-        {
-            isGrounded = false;
-        }
-    }
+    //// If player just exited touching a surface, isGrounded = false
+    //void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Surface")
+    //    {
+    //        isGrounded = false;
+    //    }
+    //}
 }
---
+++ Assets/Scripts/PlayerExplosion.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+
+public class PlayerExplosion : MonoBehaviour
+{
+    public string thisSceneName;
+
+    public float cubeSize = 0.2f;
+    public int cubesInRow = 5;
+
+    float cubesPivotDistance;
+    Vector3 cubesPivot;
+
+    public float explosionForce = 50f;
+    public float explosionRadius = 4f;
+    public float explosionUpward = 0.4f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //calculate pivot distance
+        cubesPivotDistance = cubeSize * cubesInRow / 2;
+        //use this value to create pivot vector)
+        cubesPivot = new Vector3(cubesPivotDistance, cubesPivotDistance, cubesPivotDistance);
+
+    }
+
+    private void OnCollisionEnter(Collision collide)
+    {
+        if (collide.gameObject.tag == "Obstacle")
+        {
+            Explode();
+            // Reload the same level
+            Initiate.Fade(thisSceneName, Color.black, 0.5f);
+        }
+
+    }
+
+    public void Explode()
+    {
+        // This disables the mesh renderer and makes cube invisible
+        gameObject.GetComponent<Renderer>().enabled = false;
+
+        //loop 3 times to create 5x5x5 pieces in x,y,z coordinates
+        for (int x = 0; x < cubesInRow; x++)
+        {
+            for (int y = 0; y < cubesInRow; y++)
+            {
+                for (int z = 0; z < cubesInRow; z++)
+                {
+                    CreatePiece(x, y, z);
+                }
+            }
+        }
+
+        //get explosion position
+        Vector3 explosionPos = transform.position;
+        //get colliders in that position and radius
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+        //add explosion force to all colliders in that overlap sphere
+        foreach (Collider hit in colliders)
+        {
+            //get rigidbody from collider object
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                //add explosion force to this body with given parameters
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionUpward);
+            }
+        }
+
+    }
+
+    void CreatePiece(int x, int y, int z)
+    {
+        //create piece
+        GameObject piece;
+        piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        // Set the piece color to the player color
+        piece.GetComponent<Renderer>().material.color = new Color(43f/255f, 43f/255f, 43f/255f);
+
+        //set piece position and scale
+        piece.transform.position = transform.position + new Vector3(cubeSize * x, cubeSize * y, cubeSize * z) - cubesPivot;
+        piece.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+
+        //add rigidbody and set mass
+        piece.AddComponent<Rigidbody>();
+        piece.GetComponent<Rigidbody>().mass = cubeSize;
+    }
+
+}
