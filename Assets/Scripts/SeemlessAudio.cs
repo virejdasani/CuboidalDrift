bf41340166b8a1f7cc8d1035a810d26bff4b088f
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-08-12 17:15:12 +0530

    Add audio

---
+++ Assets/Scripts/SeemlessAudio.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SeemlessAudio : MonoBehaviour
+{
+    private static SeemlessAudio instance = null;
+
+    public static SeemlessAudio Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
